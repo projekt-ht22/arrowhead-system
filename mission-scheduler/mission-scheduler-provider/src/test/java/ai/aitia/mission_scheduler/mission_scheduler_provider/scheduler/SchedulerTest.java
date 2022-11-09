148c06577e887e4aa737f297dfc455e2793d2b21
s7rul <eriser-8@student.ltu.se>
2022-11-09 12:37:10 +0100

    Added unit test framework

---
+++ mission-scheduler/mission-scheduler-provider/src/test/java/ai/aitia/mission_scheduler/mission_scheduler_provider/scheduler/SchedulerTest.java
@@ -0,0 +1,13 @@
+package ai.aitia.mission_scheduler.mission_scheduler_provider.scheduler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SchedulerTest {
+    
+    @Test
+    void testTest() {
+        assertEquals(4, 4);
+    }
+}
