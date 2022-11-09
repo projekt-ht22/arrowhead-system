ba303c327544f94ac78d71a86055bed83b6ae857
s7rul <eriser-8@student.ltu.se>
2022-11-09 12:58:30 +0100

    Added proper unit tests for scheduler

--- mission-scheduler/mission-scheduler-provider/src/test/java/ai/aitia/mission_scheduler/mission_scheduler_provider/scheduler/SchedulerTest.java
+++ mission-scheduler/mission-scheduler-provider/src/test/java/ai/aitia/mission_scheduler/mission_scheduler_provider/scheduler/SchedulerTest.java
@@ -2,12 +2,42 @@ package ai.aitia.mission_scheduler.mission_scheduler_provider.scheduler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ai.aitia.mission_scheduler.common.Mission;
+
 public class SchedulerTest {
-    
-    @Test
-    void testTest() {
-        assertEquals(4, 4);
+
+    Scheduler scheduler;
+
+    @BeforeEach
+    void newScheduler() {
+        scheduler = new Scheduler();
     }
+
+    @Test
+    void testAddingMissionsSamePriority() {
+        scheduler.addMission(new Mission(new ArrayList<String>(), "first", 0));
+        scheduler.addMission(new Mission(new ArrayList<String>(), "second", 0));
+        scheduler.addMission(new Mission(new ArrayList<String>(), "third", 0));
+
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "first");
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "second");
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "third");
+    }
+
+    @Test
+    void testAddingMissionDifferentPriority() {
+        scheduler.addMission(new Mission(new ArrayList<String>(), "low", 1));
+        scheduler.addMission(new Mission(new ArrayList<String>(), "high", 20));
+        scheduler.addMission(new Mission(new ArrayList<String>(), "middle", 3));
+
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "high");
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "middle");
+        assertEquals(scheduler.getNextAndRemoveMission().getName(), "low");
+    }
+    
 }
